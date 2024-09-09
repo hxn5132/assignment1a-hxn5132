4afa12067ca0c1c1adf2fb1beb8b8b432518c425
Hannah Nguyen <hxn5132@psu.edu>
2024-09-08 22:57:22 -0400

    Both IDs updated

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "hxn5132";
+    char STUDENT_ID[] = "937700223";
 
     return 0 ;
 }
